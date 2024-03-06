8590b1639123fa9ae2db225ad828194a8e9c75f0
crasion528 <82593848+crasion528@users.noreply.github.com>
2024-03-06 14:25:59 -0500

    Create outcomes of user vs computer choices
    used an `if else` statement to establish the outcomes
    and winner of RPS. Also added statements to be returned
    for each outcome.

---
+++ javascript.js
@@ -0,0 +1,38 @@
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0){
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = getComputerChoice();
+    
+
+    if (playerSelection === computerSelection) {
+            return "Tie!"
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")) {
+            return "Player wins! " + playerSelection + " beats " + computerSelection;
+    } else {
+            return "Computer wins! " + computerSelection + " beats " + playerSelection;  
+
+    }
+}
+
+// Game test call 
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+// Game test call end
+
+function playGame() {
+    playRound(playerSelection, computerSelection);
+}
