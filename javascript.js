648549bd786c6c4ce666e17b34daf563e97738f4
crasion528 <82593848+crasion528@users.noreply.github.com>
2024-03-31 16:07:51 -0400

    Touch up playGame() function
    deleted the function body

--- javascript.js
+++ javascript.js
@@ -28,11 +28,10 @@ function playRound(playerSelection, computerSelection) {
 }
 
 // Game test call 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
 // Game test call end
 
 function playGame() {
-    playRound(playerSelection, computerSelection);
 }
